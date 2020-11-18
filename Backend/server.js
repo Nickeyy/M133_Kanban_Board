a116c31f3587a41af73f4450833822bc21578505
Nick Durrer <nick_durrer@sluz.ch>
2020-11-18 19:17:09 +0100

    Created Put and Put Method

---
+++ Backend/server.js
@@ -0,0 +1,38 @@
+import { Application, Router, send } from "https://deno.land/x/oak@v6.3.1/mod.ts";
+
+let cards = [];
+let id = 0;
+
+const app = new Application();
+const router = new Router();
+
+router
+    .get("/cards", (context) => context.response.body = cards)
+    .post("/cards", async (context) => {
+        const card = await context.request.body({type: "json"}).value;
+        card.id = id;
+        id++;
+        cards = [
+            ...cards,
+            card
+        ]
+        context.response.body = "Created new Task";
+        context.response.status = 201;
+    })
+    .put("/cards/:id", async context => {
+        const id = context.params.id;
+        const card = await context.request.body({type: "json"}).value;
+        const oldCard = cards.find(card => card.id == id);
+        const index = cards.indexOf(oldCard);
+        card.id = parseInt(id, 10);
+        cards[index] = card
+        context.response.body = "Task updated";
+        context.response.status = 200;
+    })
+    .get("/styles/:fileName", (context) => {
+        const fileName = context.params.fileName;
+        return send(context, `../frontend/styles/${fileName}`);
+      });
+
+app.use(router.routes());    
+app.listen({port:8000});
