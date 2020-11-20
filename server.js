dbecda521e048200e0c1c3264c69825bbdb586cb
Nick Durrer <nick_durrer@sluz.ch>
2020-11-20 20:04:48 +0100

    Created delete Function and get Method for getting index.html

--- server.js
+++ server.js
@@ -29,10 +29,22 @@ router
         context.response.body = "Task updated";
         context.response.status = 200;
     })
+    .delete("/cards/:id", async context => {
+        const id = context.params.id;
+        cards = cards.filter(card => card.id != id);
+        context.response.body = "Task deleted";
+        context.response.status = 200;
+    })
     .get("/styles/:fileName", (context) => {
         const fileName = context.params.fileName;
         return send(context, `../frontend/styles/${fileName}`);
-      });
+    })
+    .get("/", async context => {
+        await send(context, context.request.url.pathname, {
+            root: `${Deno.cwd()}/frontend`,
+            index: 'index.html'
+        })
+    })
 
 app.use(router.routes());    
 app.listen({port:8000});
