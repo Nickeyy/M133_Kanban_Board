e6b9a194d4162b22aa09f12e3bc6493f395951d7
Nick Durrer <nick_durrer@sluz.ch>
2020-11-25 08:31:44 +0100

    Created Method for Dynamic Loaded Columns

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ import { Application, Router, send } from "https://deno.land/x/oak@v6.3.1/mod.ts
 
 let cards = [];
 let id = 0;
+const columns = [
+    {
+        id: 1,
+        name: 'Todo',
+        color: 'red'
+    },
+    {
+        id: 2,
+        name: 'In progress',
+        color: 'blue'
+    },
+    {
+        id: 3,
+        name: 'Done',
+        color: 'green'
+    }
+];
 
 const app = new Application();
 const router = new Router();
@@ -9,7 +26,7 @@ const router = new Router();
 router
     .get("/cards", (context) => context.response.body = cards)
     .post("/cards", async (context) => {
-        const card = await context.request.body({type: "json"}).value;
+        const card = await context.request.body({ type: "json" }).value;
         card.id = id;
         id++;
         cards = [
@@ -21,7 +38,7 @@ router
     })
     .put("/cards/:id", async context => {
         const id = context.params.id;
-        const card = await context.request.body({type: "json"}).value;
+        const card = await context.request.body({ type: "json" }).value;
         const oldCard = cards.find(card => card.id == id);
         const index = cards.indexOf(oldCard);
         card.id = parseInt(id, 10);
@@ -35,6 +52,10 @@ router
         context.response.body = "Task deleted";
         context.response.status = 200;
     })
+    .get("/columns", async context => {
+        context.response.body = columns;
+        context.response.status = 200;
+    })
     .get("/styles/:filename", async context => {
         const filename = context.params.filename;
         await send(context, `/frontend/styles/${filename}`)
@@ -46,5 +67,5 @@ router
         })
     })
 
-app.use(router.routes());    
-app.listen({port:8000});
+app.use(router.routes());
+app.listen({ port: 8000 });
